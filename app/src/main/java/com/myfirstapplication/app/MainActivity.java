3990c924539d93eb9f62e163b82019e62ae59227
Abid Shafique <mailabidalishafique@gmail.com>
2020-09-30 12:29:55 +0500

    Successfully Created button and add evnt on it and perform functionality by getting it's ID

--- app/src/main/java/com/myfirstapplication/app/MainActivity.java
+++ app/src/main/java/com/myfirstapplication/app/MainActivity.java
@@ -3,6 +3,8 @@ package com.myfirstapplication.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public void DisplayMessage(View view) {
+        TextView message = findViewById(R.id.myText);
+        TextView showText = findViewById(R.id.MessageView);
+        showText.setText((message.getText()).toString());
     }
 }
